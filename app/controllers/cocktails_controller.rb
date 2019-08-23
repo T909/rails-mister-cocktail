2a334f3f535a64f063519d9f53baf65463bd4efb
Ijoline Homphomsiltham <ijoline@gmail.com>
2019-08-23 16:12:28 +0100

    Add Photo Uploader

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -19,7 +19,7 @@ class CocktailsController < ApplicationController
   end
   private
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :photo)
   end
   def fetch_cocktail
     @cocktail = Cocktail.find(params[:id])
